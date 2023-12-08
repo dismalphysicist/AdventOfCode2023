ca9209a7bbb594b61bb8a97db50ab824ce04b2fe
Lois Flower <lois.flower@durham.ac.uk>
2023-12-08 13:40:33 +0000

    Day 4 part 2 using recursion oops, takes about a minute

--- day4.py
+++ day4.py
@@ -14,17 +14,41 @@ def score_card1(card):
     if mn in winningNums:
       matches += 1
 
-  return matches # part 1
+  return matches
+
+def score_card2(ids,cards):
+  
+  if len(ids)==0:
+    # no matches
+    return 0
+  
+  elif len(ids)==1:
+    # add 1 to total and score matches as copies
+    matches = score_card1(cards[ids[0]-1])
+    return 1 + score_card2([i for i in range(ids[0]+1,ids[0]+matches+1)],cards)
+  
+  else:
+    # need to reduce further
+    # evaluate first + rest
+    first = ids.pop(0)
+    return score_card2([first],cards) + score_card2(ids,cards)
 
 lines = []
 
 with open("input4.txt") as f:
   lines = f.read().splitlines()
 
+ids = []
 sumPoints = 0
 for card in lines:
-  matches = score_card1(card)
+  # make list of ids for part 2
+  id = parse_line(card.split(":")[0],r"\d+")[0]
+  ids.append(id)
 
+  # score points on cards for part 1
+  matches = score_card1(card)
   sumPoints += 2**(matches-1) if matches>0 else 0
 
-print(sumPoints)
+print("Answer to part 1 is {}".format(sumPoints))
+
+print("Answer to part 2 is {}".format(score_card2(ids,lines)))
